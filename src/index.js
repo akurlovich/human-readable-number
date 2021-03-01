b9d71e1912f6e3dbc81d70e378557c0efb94f1e4
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-03-01 16:50:35 +0300

    fix: function

--- src/index.js
+++ src/index.js
@@ -1,96 +1,46 @@
-module.exports = function toReadable (s) {
-  var th = ['','thousand','million', 'billion','trillion'];
-  var dg = ['zero','one','two','three','four', 'five','six','seven','eight','nine'];
-  var tn = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'];
-  var tw = ['twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
-    s = s.toString();
-    s = s.replace(/[\, ]/g,'');
-    if (s != parseFloat(s)) return 'not a number';
-    var x = s.indexOf('.');
-    if (x == -1)
-        x = s.length;
-    if (x > 15)
-        return 'too big';
-    var n = s.split(''); 
-    var str = '';
-    var sk = 0;
-    for (var i=0;   i < x;  i++) {
-        if ((x-i)%3==2) { 
-            if (n[i] == '1') {
-                str += tn[Number(n[i+1])] + ' ';
-                i++;
-                sk=1;
-            } else if (n[i]!=0) {
-                str += tw[n[i]-2] + ' ';
-                sk=1;
-            }
-        } else if (n[i]!=0) { // 0235
-            str += dg[n[i]] +' ';
-            if ((x-i)%3==0) str += 'hundred ';
-            sk=1;
-        }
-        if ((x-i)%3==1) {
-            if (sk)
-                str += th[(x-i-1)/3] + ' ';
-            sk=0;
-        }
-    }
+module.exports = function toReadable (number) {
+  let thousand = ['','thousand','million', 'billion','trillion'],
+      digits = ['zero','one','two','three','four', 'five','six','seven','eight','nine'],
+      tens = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'],
+      twentys = ['twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+  number = number.toString();
+  number = number.replace(/[\, ]/g,'');
+  if (number != parseFloat(number)) return 'not a number';
+  var x = number.indexOf('.');
+  if (x == -1)
+      x = number.length;
+  if (x > 15)
+      return 'too big';
+  var n = number.split(''); 
+  var str = '';
+  var sk = 0;
+  for (var i=0;   i < x;  i++) {
+      if ((x-i)%3==2) { 
+          if (n[i] == '1') {
+              str += tens[Number(n[i+1])] + ' ';
+              i++;
+              sk=1;
+          } else if (n[i]!=0) {
+              str += twentys[n[i]-2] + ' ';
+              sk=1;
+          }
+      } else if (n[i]!=0) { // 0235
+          str += digits[n[i]] +' ';
+          if ((x-i)%3==0) str += 'hundred ';
+          sk=1;
+      }
+      if ((x-i)%3==1) {
+          if (sk)
+              str += thousand[(x-i-1)/3] + ' ';
+          sk=0;
+      }
+  }
 
-    if (x != s.length) {
-        var y = s.length;
-        str += 'point ';
-        for (var i=x+1; i<y; i++)
-            str += dg[n[i]] +' ';
-    }
-    return str.replace(/\s+/g,' ');
+  if (x != number.length) {
+      var y = number.length;
+      str += 'point ';
+      for (var i=x+1; i<y; i++)
+          str += digits[n[i]] +' ';
+  }
+  return str.replace(/\number+/g,' ');
 }
-
-// var th = ['','thousand','million', 'billion','trillion'];
-// var dg = ['zero','one','two','three','four', 'five','six','seven','eight','nine'];
-//  var tn = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'];
-//  var tw = ['twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
-
-// function toWords(s) {
-//     s = s.toString();
-//     s = s.replace(/[\, ]/g,'');
-//     if (s != parseFloat(s)) return 'not a number';
-//     var x = s.indexOf('.');
-//     if (x == -1)
-//         x = s.length;
-//     if (x > 15)
-//         return 'too big';
-//     var n = s.split(''); 
-//     var str = '';
-//     var sk = 0;
-//     for (var i=0;   i < x;  i++) {
-//         if ((x-i)%3==2) { 
-//             if (n[i] == '1') {
-//                 str += tn[Number(n[i+1])] + ' ';
-//                 i++;
-//                 sk=1;
-//             } else if (n[i]!=0) {
-//                 str += tw[n[i]-2] + ' ';
-//                 sk=1;
-//             }
-//         } else if (n[i]!=0) { // 0235
-//             str += dg[n[i]] +' ';
-//             if ((x-i)%3==0) str += 'hundred ';
-//             sk=1;
-//         }
-//         if ((x-i)%3==1) {
-//             if (sk)
-//                 str += th[(x-i-1)/3] + ' ';
-//             sk=0;
-//         }
-//     }
-
-//     if (x != s.length) {
-//         var y = s.length;
-//         str += 'point ';
-//         for (var i=x+1; i<y; i++)
-//             str += dg[n[i]] +' ';
-//     }
-//     return str.replace(/\s+/g,' ');
-// }
-
-// console.log(toReadable('87654321'));
