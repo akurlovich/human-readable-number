9ee0a7b0402d8a46c18b88b84046f18f146645ca
akurlovich <71250705+akurlovich@users.noreply.github.com>
2021-03-01 20:03:33 +0300

    fit: function contant

--- src/index.js
+++ src/index.js
@@ -1,32 +1,47 @@
-module.exports = function toReadable (number) {
-  let thousand = ['','thousand','million', 'billion','trillion'],
-      digits = ['zero','one','two','three','four', 'five','six','seven','eight','nine'],
-      tens = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'],
-      twentys = ['twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'],
-      number = number.toString().replace(/[\, ]/g,'').split(''),
-      str = '',
-      some = 0;
-  for (let i = 0;   i < x;  i++) {
-      if ((x - i) % 3 == 2) { 
-          if (n[i] == '1') {
-              str += tens[Number(n[i + 1])] + ' ';
-              i++;
-              some = 1;
-          } else if (n[i] != 0) {
-              str += twentys[n[i] - 2] + ' ';
-              some = 1;
-          }
-      } else if (n[i] != 0) {
-          str += digits[n[i]] +' ';
-          if ((x - i) % 3 == 0) str += 'hundred ';
-          some=1;
-      }
-      if ((x - i) % 3 == 1) {
-          if (some)
-              str += thousand[(x - i - 1) / 3] + ' ';
-          some = 0;
-      }
-  }
+module.exports = function toReadable (s) {
+    let th = ['','thousand','million', 'billion','trillion'];
+    let dg = ['zero','one','two','three','four', 'five','six','seven','eight','nine'];
+    let tn = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen', 'seventeen','eighteen','nineteen'];
+    let tw = ['twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+    s = s.toString();
+    s = s.replace(/[\, ]/g,'');
+    if (s === '0') return 'zero';
+    if (s != parseFloat(s)) return 'not a number';
+    let x = s.indexOf('.');
+    if (x == -1)
+        x = s.length;
+    if (x > 15)
+        return 'too big';
+    let n = s.split(''); 
+    let str = '';
+    let sk = 0;
+    for (let i=0;   i < x;  i++) {
+        if ((x-i)%3==2) { 
+            if (n[i] == '1') {
+                str += tn[Number(n[i+1])] + ' ';
+                i++;
+                sk=1;
+            } else if (n[i]!=0) {
+                str += tw[n[i]-2] + ' ';
+                sk=1;
+            }
+        } else if (n[i]!=0) { // 0235
+            str += dg[n[i]] +' ';
+            if ((x-i)%3==0) str += 'hundred ';
+            sk=1;
+        }
+        if ((x-i)%3==1) {
+            if (sk)
+                str += th[(x-i-1)/3] + ' ';
+            sk=0;
+        }
+    }
 
-  return str.replace(/\number+/g,' ');
+    if (x != s.length) {
+        let y = s.length;
+        str += 'point ';
+        for (let i=x+1; i<y; i++)
+            str += dg[n[i]] +' ';
+    }
+    return str.replace(/\s+/g,' ').slice(0, -1)
 }
